08e1891ddf58b80ede32be3f0805afcd7815b872
kyamaguchi <kzh.yap@gmail.com>
2019-12-12 09:49:19 +0900

    Try exec

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const exec = require('@actions/exec');
+
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
@@ -10,6 +12,10 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+
+  console.log(`Commands from custom action`);
+  await exec.exec('pwd');
+  await exec.exec('ls -al');
 } catch (error) {
   core.setFailed(error.message);
 }
